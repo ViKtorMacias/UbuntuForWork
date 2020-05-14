b3f01fbb9638cc03701ce813c48afbef55d98b20
Victor Macias <juliomacias@gmail.com>
2020-05-14 16:42:49 -0500

    DevOps gitlab 1

--- Devops.py
+++ Devops.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python
 
-#import os
-#import sys, traceback
+import os
+import sys, traceback
 
 
-#if os.getuid() != 0:
-	#print "Sorry. This script requires sudo privledges"
-	#sys.exit()
+if os.getuid() != 0:
+	print "Sorry. This script requires sudo privledges"
+	sys.exit()
 def main():
 	try:
 		print ('''
@@ -25,12 +25,12 @@ def main():
  \033[1;32m+ -- -- +=[ 331 Tools DevOps\033[1;m
 
 
-\033[1;91m[W] Before updating your system , please remove all Kali-linux repositories to avoid any kind of problem .\033[1;m
+\033[1;91m[W] Before updating your system , please remove all  repositories to avoid any kind of problem .\033[1;m
 		''')
 		def inicio1():
 			while True:
 				print ('''
-1) Add repositories & Update 
+1) Source Control Management
 2) View Categories
 3) Install classicmenu indicator
 4) Install Kali menu
@@ -42,41 +42,26 @@ def main():
 			
 				while opcion0 == "1":
 					print ('''
-1) Add kali linux repositories
+1) Install and configure the necessary dependencies
 2) Update
-3) Remove all kali linux repositories
-4) View the contents of sources.list file
+3) Add the GitLab package repository
+4) Install the package GitLab
 
 					''')
 					repo = raw_input("\033[1;32mWhat do you want to do ?> \033[1;m")
 					if repo == "1":
-						cmd1 = os.system("apt-key adv --keyserver pool.sks-keyservers.net --recv-keys ED444FF07D8D0BF6")
-						cmd2 = os.system("echo '# Kali linux repositories | Added by Katoolin\ndeb http://http.kali.org/kali kali-rolling main contrib non-free' >> /etc/apt/sources.list")
+						cmd1 = os.system("sudo apt-get update -m")
+						cmd2 = os.system("sudo apt-get install -y curl openssh-server ca-certificates")
 					elif repo == "2":
 						cmd3 = os.system("apt-get update -m")
 					elif repo == "3":
-						infile = "/etc/apt/sources.list"
-						outfile = "/etc/apt/sources.list"
-
-						delete_list = ["# Kali linux repositories | Added by Katoolin\n", "deb http://http.kali.org/kali kali-rolling main contrib non-free\n"]
-						fin = open(infile)
-						os.remove("/etc/apt/sources.list")
-						fout = open(outfile, "w+")
-						for line in fin:
-						    for word in delete_list:
-						        line = line.replace(word, "")
-						    fout.write(line)
-						fin.close()
-						fout.close()
-						print ("\033[1;31m\nAll kali linux repositories have been deleted !\n\033[1;m")
+						cmd4 = os.system("curl https://packages.gitlab.com/install/repositories/gitlab/gitlab-ee/script.deb.sh | sudo bash")
 					elif repo == "back":
 						inicio1()
 					elif repo == "gohome":
 						inicio1()
 					elif repo == "4":
-						file = open('/etc/apt/sources.list', 'r')
-
-						print (file.read())
+						cmd5 = os.system("udo EXTERNAL_URL='https://gitlab.example.com' apt-get install gitlab-ee")
 
 					else:
 						print ("\033[1;31mSorry, that was an invalid command!\033[1;m") 					
